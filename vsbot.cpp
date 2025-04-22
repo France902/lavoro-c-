750601e976ec42d7297501f6de7467b2f9c50815
France902 <frascic231@gmail.com>
2025-04-23 00:13:05 +0200

    Update vsbot.cpp

--- vsbot.cpp
+++ vsbot.cpp
@@ -8,7 +8,16 @@
 #define C 31
 
 using namespace std;
-
+void guida();
+int controllo_vittoria(int si, int sj){
+	if(sj==0 && si<=6 && si>=4) return 1;
+	else{
+		if(sj==30 && si<=6 && si>=4) return 2;
+		else{
+			return 0;
+		}
+	}
+}
 void vedimatrice(char mat[R][C], bool cosa, int r, int c, int rand1, int rand2, int r2, int c2, int si_palla, int sj_palla)
 {
    int i,j;  
@@ -42,8 +51,8 @@ void vedimatrice(char mat[R][C], bool cosa, int r, int c, int rand1, int rand2, 
          {
          	if(j==0 || j==C-1){
          		 mat[i][j] = '|';
-         		if(i==R/2-1 || i==R/2+1) mat[i][j] = '-';
-         		if(i==R/2) mat[i][j] = ' ';
+         		if(i==R/2-2 || i==R/2+2) mat[i][j] = '-';
+         		if(i==R/2-1 || i==R/2 ||i==R/2+1) mat[i][j] = ' ';
          		
 			 }
 			else{
@@ -64,40 +73,41 @@ void vedimatrice(char mat[R][C], bool cosa, int r, int c, int rand1, int rand2, 
          cout << endl;   
       }   
    }                  
-   cout << endl;   
+   
 }
 
-int main(int argc, char *argv[])
+int main()
 {
 	system("color 2F");
+	system("CLS");
 	bool running = true;
     char mosse[1000]; 
-    int cont = 0;
+    int cont = 0, punto_g=0, punto_c=0, vitt=0;
 	srand(time(0));
     char matrice[R][C]; 
     int si=5,sj=1, n, n2, si2=5, sj2=28, si_palla = R/2, sj_palla = C/2;
     int conta=0, conta2=0, rand1, rand2;
+    char mod;
     do{
         rand1 = rand()%10;
     	rand2 = rand()%20;	
 	}while(rand1 == 4 && rand2 == 4);
-
-    
     vedimatrice(matrice,false,si,sj, rand1, rand2, si2, sj2, si_palla, sj_palla);
-
     char scelta='v';
-
+	cout<<"Benvenuto in FIFA++! ";
+	cout<<"Scegliere la modalita': "<<endl<<"1- Giocatore vs computer "<<endl<<"2- Giocatore contro Giocatore"<<endl<<"3- Comandi"<<endl;
+	do{
+		mod = getch();
+	} while(mod!= '1' && mod!='2' && mod != '3');
+	if(mod == '3') guida();
+	if(mod=='1'){
     while(running)
     {
        system("CLS");
 
-       vedimatrice(matrice,true,si,sj, rand1, rand2, si2, sj2, si_palla, sj_palla);
-       
-       cout << "w-sopra  s-sotto  a-sinistra  d-destra  q-alto-sinistra  e-alto-destra  x-basso-destra  z-basso-sinistra" << endl;                
-       cout << "f-fine" << endl;                          
-       cout << endl << "Scelta :";                
+       vedimatrice(matrice,true,si,sj, rand1, rand2, si2, sj2, si_palla, sj_palla);              
        scelta = 'b';
-           
+           cout<<"Giocatore: "<<punto_g<<"         Computer: "<<punto_c;
        
         if (_kbhit()) {
 			scelta = getch();
@@ -154,8 +164,10 @@ int main(int argc, char *argv[])
                 conta2++;
              }
              if(si2 == si_palla && sj2 == sj_palla){
+             if(si_palla != si+1 || sj2!=sj){
              	if(si_palla>1) si_palla--;
              	 else si_palla++;
+             	 } else si2++;
 			 }
 			 
              break;
@@ -168,8 +180,10 @@ int main(int argc, char *argv[])
                 conta2++;
              }
 			  if(si2 == si_palla && sj2 == sj_palla ){
+			  if(si_palla != si-1 || sj2!=sj){
 			  	if(si_palla<R-2) si_palla++;
              	 else si_palla--;
+             	 } else si2--;
 			  }       
              break;
           }                           
@@ -181,8 +195,10 @@ int main(int argc, char *argv[])
                 conta2++;
              }
 			  if(si2 == si_palla && sj2 == sj_palla ){
-					if(sj_palla>1) sj_palla--;
+			  	if(sj_palla != sj+1 || si2!=si) {
+					if(sj_palla>1 || (si_palla <=6 && si_palla>=4)) sj_palla--;
              	 	else sj_palla++;
+             	 	} else sj2++;
 				}  
              break;
           }                           
@@ -194,8 +210,10 @@ int main(int argc, char *argv[])
                 conta2++;
              }
 			 if(si2 == si_palla && sj2 == sj_palla ){
-			    	if(sj_palla<C-2) sj_palla++;
+			 if(sj_palla != sj-1 || si2!=si){
+			    	if(sj_palla<C-2 || (si_palla <=6 && si_palla>=4)) sj_palla++;
              	 	else sj_palla--;
+             	 	} else sj2--;
 				}     
              break;
           }
@@ -208,7 +226,8 @@ int main(int argc, char *argv[])
                 conta2++;
              }
 			  if(si2 == si_palla && sj2 == sj_palla){
-			  	if(si_palla>1 && sj_palla>1){
+			  if(si_palla != si+1 || sj_palla != sj+1){
+			  	if(si_palla>1 && sj_palla>1 || (si_palla <=6 && si_palla>=4)){
 			  		si_palla--;
 			  		sj_palla--;
 				  } 
@@ -216,8 +235,11 @@ int main(int argc, char *argv[])
              		si2++;
              		sj2++;
 				 }
-			  	
 			  } 
+			  } else{
+					si2++;
+					sj2++;
+				}
              break;
           }
 		  case 'o':
@@ -229,7 +251,8 @@ int main(int argc, char *argv[])
                 conta2++;
              }
              if(si2 == si_palla && sj2 == sj_palla){
-			  	if(si_palla>1 && sj_palla<C-2){
+             	if(si_palla != si+1 || sj_palla != sj-1){
+			  	if(si_palla>1 && sj_palla<C-2 || (si_palla <=6 && si_palla>=4)){
 			  		si_palla--;
 			  		sj_palla++;
 				  } 
@@ -237,8 +260,12 @@ int main(int argc, char *argv[])
              		si2++;
              		sj2--;
 				 }
+				 
 			  }  
-			      
+			   } else{
+					si2++;
+					sj2--;
+				}   
              break;
           }
           
@@ -251,7 +278,8 @@ int main(int argc, char *argv[])
                 conta2++;
              }   
               if(si2 == si_palla && sj2 == sj_palla){
-			  	if(si_palla<R-2 && sj_palla<C-2){
+              if(si_palla != si-1 || sj_palla != sj-1){
+			  	if(si_palla<R-2 && sj_palla<C-2 || (si_palla <=6 && si_palla>=4)){
 			  		si_palla++;
 			  		sj_palla++;
 				  } 
@@ -259,7 +287,12 @@ int main(int argc, char *argv[])
              		si2--;
              		sj2--;
 				 }
+			
 			  }
+			  	 } else{
+					si2--;
+					sj2--;
+				}
              break;
           }
 		  case 'n':
@@ -271,7 +304,8 @@ int main(int argc, char *argv[])
                 conta2++;
              }   
               if(si2 == si_palla && sj2 == sj_palla){
-			  	if(si_palla<R-2 && sj_palla>1){
+              if(si_palla != si-1 || sj_palla != sj+1){
+			  	if(si_palla<R-2 && sj_palla>1 || (si_palla <=6 && si_palla>=4)){
 			  		si_palla++;
 			  		sj_palla--;
 				  } 
@@ -279,6 +313,10 @@ int main(int argc, char *argv[])
              		si2--;
              		sj2++;
 				 }
+				 } else{
+					si--;
+					sj++;
+				}
 			  }
              break;
           }           
@@ -332,7 +370,7 @@ int main(int argc, char *argv[])
              }
 				if(si == si_palla && sj == sj_palla ){
 					if(sj_palla != sj2+1 || si!=si2) {
-					if(sj_palla>1) sj_palla--;
+					if(sj_palla>1 || (si_palla <=6 && si_palla>=4)) sj_palla--;
              	 	else sj_palla++;
              	 	} else sj++;
 				}  
@@ -347,7 +385,7 @@ int main(int argc, char *argv[])
              }
 			    if(si == si_palla && sj == sj_palla ){
 			    	if(sj_palla != sj2-1 || si!=si2){
-			    	if(sj_palla<C-2) sj_palla++;
+			    	if(sj_palla<C-2 || (si_palla <=6 && si_palla>=4)) sj_palla++;
              	 	else sj_palla--;
              	 	} else sj--;
 				} 
@@ -363,7 +401,7 @@ int main(int argc, char *argv[])
              }
 			  if(si == si_palla && sj == sj_palla){
 			  	if(si_palla != si2+1 || sj_palla != sj2+1){
-			  		if(si_palla>1 && sj_palla>1){
+			  		if(si_palla>1 && sj_palla>1 || (si_palla <=6 && si_palla>=4)){
 			  		si_palla--;
 			  		sj_palla--;
 				  } 
@@ -387,8 +425,8 @@ int main(int argc, char *argv[])
                 conta++;
              }
 			  if(si == si_palla && sj == sj_palla){
-			  	if(si_palla != si2+1 || sj_palla != sj2-1){
-			  	if(si_palla>1 && sj_palla<C-2){
+			  	if(si_palla != si2+1 || sj_palla != sj2-1 ){
+			  	if(si_palla>1 && sj_palla<C-2 || (si_palla <=6 && si_palla>=4)){
 			  		si_palla--;
 			  		sj_palla++;
 				  } 
@@ -413,7 +451,7 @@ int main(int argc, char *argv[])
              }   
               if(si == si_palla && sj == sj_palla){
               	if(si_palla != si2-1 || sj_palla != sj2-1){
-			  	if(si_palla<R-2 && sj_palla<C-2){
+			  	if(si_palla<R-2 && sj_palla<C-2 || (si_palla <=6 && si_palla>=4)){
 			  		si_palla++;
 			  		sj_palla++;
 				  } 
@@ -438,7 +476,7 @@ int main(int argc, char *argv[])
              }   
               if(si == si_palla && sj == sj_palla){
               	if(si_palla != si2-1 || sj_palla != sj2+1){
-			  	if(si_palla<R-2 && sj_palla>1){
+			  	if(si_palla<R-2 && sj_palla>1 || (si_palla <=6 && si_palla>=4)){
 			  		si_palla++;
 			  		sj_palla--;
 				  } 
@@ -455,31 +493,42 @@ int main(int argc, char *argv[])
           } 
 		                     
        }
-        Sleep(300); 
+       vitt = controllo_vittoria(si_palla, sj_palla);
+       if(vitt==1) {
+       	si=5;
+		sj=1;
+		si2=5;
+		sj2=28; 
+		si_palla = R/2; 
+		sj_palla = C/2;
+		punto_c++;
+	   }
+       else if(vitt==2) {
+       	si=5;
+		sj=1;
+		si2=5;
+		sj2=28; 
+		si_palla = R/2; 
+		sj_palla = C/2;
+		 punto_g++;
+	   }
+        Sleep(250); 
     }
+}
        
        
-	   if(si == rand1 && sj == rand2){
-	   	conta2++;
-	   	do{
-		rand1 = rand()%10;
-		rand2 = rand()%20;
-		}while((rand1==si && rand2==sj) or ((rand1==si2 && rand2==sj2)));
-	}
-	else{
-		if(si2 == rand1 && sj2 == rand2){
-	   	conta2++;
-	   	do{
-		rand1 = rand()%10;
-		rand2 = rand()%20;
-		}while((rand1==si2 && rand2==sj2)or (rand1==si && rand2==sj));
-	}                  
-    }
-
-
-	            
+                        
     cout << endl;
         
     system("PAUSE");
     return EXIT_SUCCESS;
+}
+void guida(){
+	system("CLS");
+	cout<<"Giocatore 1 (sinistra): "<<endl<<"W: Spostamento in alto     S: Spostamento in basso     D: Spostamento a destra      A: Spostamento a sinistra"<<endl;
+	cout<<"Q: Spostamento obliquo a sinistra in alto     E: Spostamento obliquo a destra in alto     Z: Spostamento obliquo a sinistra in basso     X: Spostamento obliquo a destra in basso"<<endl;
+	cout<<"Giocatore 2 (destra): "<<endl<<"I: Spostamento in alto     K: Spostamento in basso     L: Spostamento a destra      J: Spostamento a sinistra"<<endl;
+	cout<<"U: Spostamento obliquo a sinistra in alto     O: Spostamento obliquo a destra in alto     N: Spostamento obliquo a sinistra in basso     M: Spostamento obliquo a destra in basso"<<endl;
+	system("PAUSE");
+	main();
 }
